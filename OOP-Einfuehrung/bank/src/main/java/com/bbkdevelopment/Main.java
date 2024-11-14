064597d2cc412607b27c08058e4e09b707ea02f5
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2024-11-14 23:30:52 +0100

    feat: add bank

--- OOP-Einfuehrung/bank/src/main/java/com/bbkdevelopment/Main.java
+++ OOP-Einfuehrung/bank/src/main/java/com/bbkdevelopment/Main.java
@@ -13,7 +13,7 @@ public final class Main {
   public static void main(String[] args) {
     final var konto = new Konto(123456, new Bank("Sparkasse", 255671), new Kunde("Max Mustermann"));
     konto.einzahlen(1000);
-    konto.auszahlen(500);
+    konto.auszahlen(600);
     System.out.println(
         "Kontostand von " + konto.liefereInhabername() + ": " + konto.liefereKontostand()
             + " Cent");
